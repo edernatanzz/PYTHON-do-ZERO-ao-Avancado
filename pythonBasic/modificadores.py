a66da8aa2a7de3d0accedb10d1e490e6c21307a9
Eder Natan <edernatan124@gmail.com>
2023-03-24 20:11:13 -0300

    .

---
+++ pythonBasic/modificadores.py
@@ -0,0 +1,8 @@
+x= str(4)
+y= int(6)
+z= float(7)
+
+print(x +x)
+print(y+y)
+print(z+z)
+
---
+++ pythonBasic/string-integers.py
@@ -0,0 +1,8 @@
+#eder natan tem 21 anos de idade e mora na cidade de vitoria da conquista.
+
+name= 'Eder Natan'
+idade= 21
+cidade='Vitoria da Conquista'
+
+print ('0' + name + 'tem '+idade + 'e mora na cidade de ' + cidade + '.')
+
