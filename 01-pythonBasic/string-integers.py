9e4f01dc9916fec472d9da73263d8f5af746953d
Eder Natan <edernatan124@gmail.com>
2023-03-26 23:07:58 -0300

    input

---
+++ 01-pythonBasic/string-integers.py
@@ -0,0 +1,9 @@
+#eder natan tem 21 anos de idade e mora na cidade de vitoria da conquista.
+
+name= 'Eder Natan'
+idade= 21
+idade=str(idade)
+cidade='Vitoria da Conquista'
+
+print (' 0 ' + name + ' tem ' + idade + ' anos de idade e mora na cidade de ' + cidade + '.')
+#print ( name + ' tem ' + str(idade) + ' anos de idade e mora na cidade de ' + cidade + '.')
---
+++ 01-pythonBasic/use-to-input.py
@@ -0,0 +1,8 @@
+name= input('qual seu nome ')
+idade= 21
+idade=str(idade)
+cidade='Vitoria da Conquista'
+
+print ( name + ' tem ' + idade + ' anos de idade e mora na cidade de ' + cidade + '.')
+#print ( name + ' tem ' + str(idade) + ' anos de idade e mora na cidade de ' + cidade + '.')
+
