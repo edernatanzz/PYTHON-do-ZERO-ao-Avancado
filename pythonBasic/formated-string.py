8b673a17d42b1fbcf1c427ee7afabd245b2af381
Eder Natan <edernatan124@gmail.com>
2023-03-28 14:15:29 -0300

    metodos para string

---
+++ pythonBasic/formated-string.py
@@ -0,0 +1,11 @@
+nome= 'Eder '
+sobrenome= 'Natan'
+profisssao = 'Programador'
+
+texto= nome+sobrenome +' e um excelente' +  '[' + profisssao + ']';
+
+print(texto)
+
+texto2= f'{nome} {sobrenome } e um excelente [{profisssao}]'
+print(texto2)
+#resultado = fulano e um excelente  [Programador]  
---
+++ pythonBasic/use-all-slice.py
@@ -0,0 +1,10 @@
+fruta = 'Abacate' 
+#index   0123456
+
+
+valor= 99.75
+valor=str(valor)
+#index 01234
+
+
+print(valor[3:5])
