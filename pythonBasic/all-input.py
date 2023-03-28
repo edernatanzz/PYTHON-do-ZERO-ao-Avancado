4190c96257d514743434a08991fb46ba58c1e33c
Eder Natan <edernatan124@gmail.com>
2023-03-28 13:23:53 -0300

    input

---
+++ pythonBasic/all-input.py
@@ -0,0 +1,7 @@
+name= input('qual seu nome ')
+idade= 21
+idade=str(idade)
+cidade='Vitoria da Conquista'
+
+print ( name + ' tem ' + idade + ' anos de idade e mora na cidade de ' + cidade + '.')
+
---
+++ pythonBasic/modificadores.py
@@ -0,0 +1,11 @@
+
+
+x= str(4)
+y= int(6)
+z= float(7)
+
+print(x +x)
+print(y+y)
+print(z+z)
+
+
