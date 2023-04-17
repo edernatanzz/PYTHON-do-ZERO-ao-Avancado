fece504d747b504db8005147f42132388888652d
Eder Natan <edernatan124@gmail.com>
2023-04-17 20:21:26 -0300

    comparar valores entra x e y

---
+++ 02-operadores/op-comparacao.py
@@ -0,0 +1,6 @@
+# multiplique comparison operators ( multiplos operadores de comparação )
+
+valor = 35
+valor2= "Produto foi aceito " if 20 <= valor < 40 else "produto nao aceito"
+
+print (valor2)
