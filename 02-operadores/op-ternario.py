e3aeadf14d1495351f8993ce7a4c90b093889573
Eder Natan <edernatan124@gmail.com>
2023-04-17 20:12:15 -0300

    operador ternario

---
+++ 02-operadores/op-ternario.py
@@ -0,0 +1,12 @@
+idade = 18
+
+###
+#if idade >= 16 :
+ #   resultado =  print("voto permitido ")
+#else:
+   # resultado = print("voto nao permitido ")
+
+resultado = 'voto permitido' if idade>= 16 else "Voto nao Permitido"
+###
+
+print(resultado)
