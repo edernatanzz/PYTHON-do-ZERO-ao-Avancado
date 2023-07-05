f8c81eca838ea3adeadee2a2bd24a585a430cec6
Eder Natan <edernatan124@gmail.com>
2023-07-05 10:04:26 -0300

    funcoes

---
+++ 03-funcoes/functions.py
@@ -0,0 +1,16 @@
+# funções
+# dry - don´t repeat youserlf
+
+
+#não repita você mesmo. 
+# #função não se repete
+def boas_vindas():
+    print("Bem vindo !")
+    print('temps 5 laptops tops em estoque')
+    
+boas_vindas() 
+
+    
+
+
+
