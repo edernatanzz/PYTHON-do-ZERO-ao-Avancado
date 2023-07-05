ea19596c43f9445fc6cc35c7093a94b909e4471e
Eder Natan <edernatan124@gmail.com>
2023-07-05 10:14:31 -0300

    function soma

---
+++ 03-funcoes/function_soma.py
@@ -0,0 +1,11 @@
+# ....
+
+def somar_dois_numeros():
+    numero1 = 10
+    numero2 = 5
+    resultado = numero1 + numero2
+    return print(f'{resultado }');
+
+
+somar_dois_numeros()
+somar_dois_numeros()
