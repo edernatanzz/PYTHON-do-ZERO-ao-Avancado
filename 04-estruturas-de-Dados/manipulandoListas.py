54c7368a863f4261c59c0d43ed942aa4fe88be1c
Eder Natan <edernatan124@gmail.com>
2023-07-15 19:39:18 -0300

    index lista

---
+++ 04-estruturas-de-Dados/manipulandoListas.py
@@ -0,0 +1,20 @@
+#armazenar masi de uma informação em variaveis
+#manter a sequencia dos dados em uma variavel 
+
+city1='Rio de Janeiro'
+city2='Sao Paulo'    
+city3='Salvador'
+
+
+#            -4                -3           -2         -1
+citys =['Rio de Janeiro ', 'Sao Paulo', 'Salvador', 'Goiania']
+#             0                 1            2          3
+numeros = [2,4,6]
+
+
+print(citys[2], citys[-3])
+
+
+citys[0] = 'Brasilia/'
+print(citys)
+
