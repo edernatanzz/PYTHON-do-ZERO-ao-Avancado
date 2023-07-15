65bbd1fa3599766f2fbbb441967ae4f7733ee971
Eder Natan <edernatan124@gmail.com>
2023-07-15 19:28:21 -0300

    listas

---
+++ 04-estruturas-de-Dados/lista.py
@@ -0,0 +1,10 @@
+#armazenar masi de uma informação em variaveis
+#manter a sequencia dos dados em uma variavel 
+
+city1='Rio de Janeiro'
+city2='Sao Paulo'    
+city3='Salvador'
+
+citys =['Rio de Janeiro ', 'Sao Paulo', 'Salvador']
+print(citys)
+
