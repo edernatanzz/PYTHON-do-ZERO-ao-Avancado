6e4198995b7ded947f5c12d5006d1530177dbb90
Eder Natan <edernatan124@gmail.com>
2023-07-15 18:57:41 -0300

    varios argumentos

---
+++ 03-funcoes/function_argumentoxargs.py
@@ -0,0 +1,8 @@
+#criar uma função que armazena numeros e strings (dados)
+def agencia(**carro):
+    return carro
+
+
+print(agencia(marca='gol ',cor='branco',motor=1.0, placa=1234))
+print(agencia(marca='gol ',cor='azul',motor=1.0, ))
+print(agencia(marca='gol ',cor='preto',motor=1.0, placa=1444))
