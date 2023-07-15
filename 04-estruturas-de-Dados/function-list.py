00235d49028fe0f3830f7476f49b79dd62a332ca
Eder Natan <edernatan124@gmail.com>
2023-07-15 19:50:05 -0300

    function in list

---
+++ 04-estruturas-de-Dados/function-list.py
@@ -0,0 +1,22 @@
+#armazenar masi de uma informação em variaveis
+#manter a sequencia dos dados em uma variavel 
+
+city1='Rio de Janeiro'
+city2='Sao Paulo'    
+city3='Salvador'
+
+
+#            -4                -3           -2         -1
+citys =['Rio de Janeiro ', 'Sao Paulo', 'Salvador', 'Goiania']
+#             0                 1            2          3
+numeros = [2,4,6]
+
+#citys.append('Santa Catarina')
+#citys.remove(citys[2]) / citys.remove('Salvador')
+#citys.insert(1,'Santa Catarina')
+#citys.pop(0)
+citys.sort()
+
+
+print(citys)
+
