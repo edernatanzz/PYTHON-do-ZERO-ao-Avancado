2af6eb4867db7e7c42b8c59f3b310e046bb1ac23
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-07-18 15:03:22 +0000

    for looping in list

---
+++ 04-estruturas-de-Dados/looping_in_list.py
@@ -0,0 +1,9 @@
+#listas
+#Armazenar informações em variaveis 
+#manter a sequencia dos dados em uma variavel
+
+valores=[50, 110,150,170]
+
+for x in valores:
+    print(f'o valor final do produto é de R$ {x}.')
+
