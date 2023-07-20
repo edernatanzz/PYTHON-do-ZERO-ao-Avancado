8441fd9f8adb14f97f75eddab38f6c1d2e1445dc
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-07-20 00:50:37 +0000

    tuple introducion

---
+++ 04-estruturas-de-Dados/tuples.py
@@ -0,0 +1,13 @@
+#Turples 
+  #armazenar mais de uma informação em variaveis
+  #manter a sequencia dos dados em uma variavel 
+  #Não pode ser alteradas(Immutable)
+
+cores_list=('amarelo','verde','azul', 'vermelho')
+cores_tuple = ('amarelo','verde','azul','vermelho')
+
+cores_tuple.append('Roxo')
+
+
+
+
