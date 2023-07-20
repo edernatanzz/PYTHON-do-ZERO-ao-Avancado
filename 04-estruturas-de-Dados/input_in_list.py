574ad4499d5fb63b5fb1513c2100cd163afde46d
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-07-20 00:32:14 +0000

    input in list

---
+++ 04-estruturas-de-Dados/input_in_list.py
@@ -0,0 +1,9 @@
+#criar uma lista de frutas a partir do input pelo usuario 
+
+frutas_usuario= input('Favor o nome das frutas separados por virgula : ')
+frutas_lista=frutas_usuario.split(', ')      #split= separar lista 
+
+
+
+print(list(frutas_lista))
+
