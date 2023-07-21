2a34e962bb3f5373a82b3d2e57e1d6bd4cec69aa
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-07-21 00:14:59 +0000

    arrays

---
+++ 04-estruturas-de-Dados/arrays.py
@@ -0,0 +1,24 @@
+# array = quando a lista é muito grande
+#array = Matriz
+#similar a listas
+#Melhor performace no cod
+
+
+from array import array
+
+letras= ['a', 'b', 'c', 'd']
+numeros_i=['10', '20', '30', '40']
+numeros_f=['1.2', '2.2', '3.2']
+
+print(letras)
+print(numeros_i)
+print(numeros_f)
+print()
+
+
+letras= array('u', ['a', 'b', 'c', 'd'])
+#numeros_i=array('i', ['10', '20', '30', '40'])
+numeros_f= array('f', ['1.2', '2.2', '3.2'] )
+print(letras)
+print(numeros_i)
+print(numeros_f)
