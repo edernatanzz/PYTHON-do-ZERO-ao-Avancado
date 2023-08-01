a47b39b8bbf6dcd5c9a070bb6fe41f73e703c056
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-08-01 23:49:26 +0000

    dicionarios

---
+++ 04-estruturas-de-Dados/dicionarios.py
@@ -0,0 +1,10 @@
+
+#Dicionarios
+# ultliza index no gformato de keys e values
+#Aceita String,Integer,float,boolean...
+
+aluno = {'nome': 'Eder', 'idade':'21y', 'nota_final': 'A', 'aprovacao': True}
+          #key    # value
+
+print (aluno)
+
