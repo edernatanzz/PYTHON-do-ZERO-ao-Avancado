9b28a935b2075ee41f2e0b21fdf7a639fa188e4e
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-08-02 00:10:28 +0000

    loop in dinionarys

---
+++ 04-estruturas-de-Dados/dicionario_att_itens.py
@@ -0,0 +1,10 @@
+
+
+aluno = {'nome': 'Eder', 'idade':'21y', 'nota_final': 'A', 'aprovacao': True}
+
+#aluno.update({'endereco ' : 'Rua A'})
+
+
+
+
+print(aluno.get('endereco', 'Inexistente'))
---
+++ 04-estruturas-de-Dados/looping_in_dicionary.py
@@ -0,0 +1,4 @@
+aluno = {'nome': 'Eder', 'idade':'21y', 'nota_final': 'A', 'aprovacao': True}
+
+for keys, values in aluno.items(): 
+    print(keys,values)
