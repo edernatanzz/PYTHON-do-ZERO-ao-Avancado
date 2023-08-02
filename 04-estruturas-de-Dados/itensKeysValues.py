584991e2e14beeb1c8be6bc046b5ab1394f0ab7f
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-08-02 01:32:26 +0000

    itens keys dict

---
+++ 04-estruturas-de-Dados/itensKeysValues.py
@@ -0,0 +1,18 @@
+
+
+aluno = {
+    'nome': 'Eder',
+    'idade':'21y',
+    'nota_final': 'A', 
+    'aprovacao': True,
+    'materias': ['fisica', 'matematica', 'Ingles']
+    
+
+}
+
+
+print(aluno.items)
+print(aluno.keys())
+print(len(aluno))
+
+print(aluno.values())
