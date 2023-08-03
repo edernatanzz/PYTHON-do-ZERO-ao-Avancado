c4c631dd148834eb8c2edae6cbcb19c15a38a572
EderNatan.dev <102772770+edernatanzz@users.noreply.github.com>
2023-08-03 23:12:56 +0000

    lambda

---
+++ 04-estruturas-de-Dados/lambda_function.py
@@ -0,0 +1,14 @@
+ #Lambda function
+ #pode ter varioas argumentos mas somnete 1 expressão 
+ #muito ultilizado dentro de outras funções
+ #CODIGO MAIS CLEAR 
+
+#def somar (x):
+ #   return x + 10 
+
+#print(somar(2))
+
+
+somar10 = lambda x,y : x + y + 10 
+print(somar10(2, 4))
+
