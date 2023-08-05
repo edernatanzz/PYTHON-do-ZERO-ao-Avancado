567df2199db601362ed63e4a08df7a0fe4d7de11
Eder Natan <edernatan124@gmail.com>
2023-08-05 17:52:25 -0300

    desafio1

---
+++ 08-desafios/ponto_steak.py
@@ -0,0 +1,15 @@
+x= input('digite a temperatura')
+
+
+if x >= 120 :
+    print ('rare')
+elif x >= 130 :
+    print ( 'medim rare')
+elif x >= 140 :
+    print('medium ')
+elif x >= 150 :
+    print('medium well')
+elif x >= 160:
+    print('well done ')
+    
+    print(x)
