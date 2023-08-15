c8a7c1e92c45ab6a2e2b88ddd0dfd4caf53a4619
EderNatan.dev <edernatan124@gmail.com>
2023-08-15 18:56:25 -0300

    deafio 03

---
+++ Desafios/desafio3.py
@@ -0,0 +1,9 @@
+num1 = int(10)
+num2 = float(3.5)
+
+def divisao ():
+     print(f'a divisão entre os 2 numeros são : {num1 / num2} com num1 de tipo {type(num1)} e num2 com tipo {type(num2)} ')
+
+
+divisao()
+
