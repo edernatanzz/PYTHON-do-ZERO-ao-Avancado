b5855f1536265b681386602cb9abd5e3d61ee161
EderNatan.dev <edernatan124@gmail.com>
2023-11-04 19:25:39 -0300

    desafio 9

---
+++ Desafios/desafio8.py
@@ -0,0 +1,9 @@
+lista =['maca', 'banana', 'manga' , 'uva']
+
+lista[1:3] = ['abacaxi ', 'abacate']
+
+
+#lista[1] = 'morango'
+#lista.append('final append')
+
+print(lista)
---
+++ Desafios/desafio9.py
@@ -0,0 +1,6 @@
+lista =['maca', 'banana', 'manga' , 'uva']
+
+
+lista.remove('banana')
+del lista[-1]
+print(lista)
